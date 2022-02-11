e5bb840f8b076769a5f3218431f99860ac253fa6
Cenk <muzzyathome@gmail.com>
2022-02-11 15:16:44 +0000

    reverseAllWords code implemented in exercise001

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -36,7 +36,12 @@ function reverseWord(word) {
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
+
+  for(var i=0; i< words.length; i++){
+    words[i] = reverseWord(words[i]);
+  }
+
+  return words;
 }
 
 function countLinuxUsers(users) {
