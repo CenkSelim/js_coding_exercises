956da087cd7533c2bd6072f689e157fa007f6d42
Cenk <muzzyathome@gmail.com>
2022-02-13 18:46:31 +0000

    summultiples code and test implemented in exercise006

---
+++ test/exercise006.test.js
@@ -0,0 +1,18 @@
+const {
+  sumMultiples
+} = require("../challenges/exercise006");
+
+
+describe("sumMultiples", () => {
+    test("it throws an error if not passed an array", () => {
+        expect(() => {
+            sumMultiples();
+        }).toThrow("arr is required");
+    });
+    test("it return the sum of any numbers which are a multiple of 3 or 5", () => {
+        const result = sumMultiples([1,3,5]);
+        const expected = 8;
+        expect(result).toBe(expected);
+        expect(sumMultiples([5, 3, 7, 8, 1, 10], 7)).toBe(18);
+    });
+});
