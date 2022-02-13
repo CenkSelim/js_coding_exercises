dfade66aed25d38ee6b75fd9e96b3867975c5cec
Cenk <muzzyathome@gmail.com>
2022-02-13 13:14:07 +0000

    all (for loop) converted to (foreach) code implemented in exercise004

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -2,9 +2,9 @@ function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   const newNums = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 1) newNums.push(nums[i]);
-  }
+  nums.forEach(function(item) {
+    if (item < 1) newNums.push(item);
+  });
   return newNums;
 }
 
@@ -13,9 +13,9 @@ function findNamesBeginningWith(names, char) {
   if (!char) throw new Error("char is required");
   const newNames = [];
 
-  for (let i = 0; i < names.length; i++) {
-    if (names[i].substring(0,1).toLowerCase() === char.toLowerCase()) newNames.push(names[i]);
-  }
+  names.forEach(function(item) {
+    if (item.substring(0,1).toLowerCase() === char.toLowerCase()) newNames.push(item);
+  });
   return newNames;
 }
 
@@ -23,9 +23,9 @@ function findVerbs(words) {
   if (!words) throw new Error("words is required");
   const verbs = [];
 
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].substring(0,3).toLowerCase() === "To ".toLowerCase()) verbs.push(words[i]);
-  }
+  words.forEach(function(item) {
+    if (item.substring(0,3).toLowerCase() === "To ".toLowerCase()) verbs.push(item);
+  });
   return verbs;
 }
 
@@ -33,9 +33,9 @@ function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   const newNums = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (Number.isInteger(nums[i])) newNums.push(nums[i]);
-  }
+  nums.forEach(function(item) {
+    if (Number.isInteger(item)) newNums.push(item);
+  });
   return newNums; 
 }
 
@@ -43,9 +43,9 @@ function getCities(users) {
   if (!users) throw new Error("users is required");
   const cities = [];
 
-  for (let i = 0; i < users.length; i++) {
-    cities.push(users[i].data.city.displayName);
-  }
+  users.forEach(function(item) {
+     cities.push(item.data.city.displayName);
+  });
   return cities;
 }
 
@@ -53,9 +53,9 @@ function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   const squareRoots = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    squareRoots.push(Math.round(Math.sqrt(nums[i])*100)/100);
-  }
+  nums.forEach(function(item) {
+    squareRoots.push(Math.round(Math.sqrt(item)*100)/100);
+  });
   return squareRoots;
 }
 
@@ -64,9 +64,9 @@ function findSentencesContaining(sentences, str) {
   if (!str) throw new Error("str is required");
   const newSentences = [];
 
-  for (let i = 0; i < sentences.length; i++) {
-    if (sentences[i].toLowerCase().indexOf(str.toLowerCase()) !== -1) newSentences.push(sentences[i]);
-  }
+  sentences.forEach(function(item) {
+    if (item.toLowerCase().indexOf(str.toLowerCase()) !== -1) newSentences.push(item);
+  });
   return newSentences;
 }
 
@@ -74,9 +74,9 @@ function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
   const longestSides = [];
 
-  for (let i = 0; i < triangles.length; i++) {
-    longestSides.push(Math.max(...triangles[i]));
-  }
+  triangles.forEach(function(item) {
+    longestSides.push(Math.max(...item));
+  });  
   return longestSides;
 }
 
