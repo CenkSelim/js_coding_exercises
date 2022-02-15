3665e9e1bb6cfe1b41cfffc76fe077f8753ca1b6
Cenk <muzzyathome@gmail.com>
2022-02-15 14:52:02 +0000

    sumDigits test and code implemented in exercise007

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -8,6 +8,9 @@ describe("sumDigits", () => {
     });
     test("should return the sum of all the digits of the number supplied", () => {
         expect(sumDigits(2)).toBe(2);
+        expect(sumDigits(1234)).toBe(10);
+        expect(sumDigits(222222)).toBe(12);
+        expect(sumDigits(9876543210)).toBe(45);
     });
     
 });
